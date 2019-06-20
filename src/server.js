50805c01158b4179c5f4eff8a69a94c83b7f8f2a
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2019-06-20 18:05:00 +0300

    feat: change server for messanger and add mongoDB

--- src/server.js
+++ src/server.js
@@ -1,80 +1,117 @@
-// const WebSocket = require('ws');
-const Db = require('./db');
-const db = new Db();
-
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 const server = require('http').createServer();
-
 const io = require('socket.io')(server);
 
-io.on('connection', (client) => {
-  db.clients[client.id] = client;
+mongoose.connect('mongodb://localhost/messDB', { useNewUrlParser: true });
 
-  client.send('message from server')
-
-    client.on('message', function incoming(data) {
-
-      let stringData = data.toString();
-      if (stringData[0] === '#') {
-        if (!db.keys[stringData.match(/#[a-z]+/i)]) {
-          db.clients[client.remotePort].send('Invalid command, list of commands:\n' + Object.keys(db.keys).join(',\n'));
-          return false;
-        } else {
-          db.keys[stringData.match(/#[a-z]+/i)](stringData.trim(), client);
-        }
-        return true;
-      } else if (db.chatRooms.length > 1) {
-        let indexRoom;
-        db.chatRooms.find((el, index) => {
-          if (el[client.remotePort]) {
-            indexRoom = Object.keys(db.chatRooms[index]);
-            return true;
-          }
-        });
-        indexRoom.forEach(element => {
-          if (element != client.remotePort) {
-            db.clients[element].send(client + ': ' + stringData);
-          }
-        });
-      } else {
-        client.send('Choose your interlocutor:\n' + Object.keys(db.clients));
-      }
-    });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('db connected');
 });
 
-io.listen(8000);
+const userSchema = new mongoose.Schema({
+  name: String,
+  lastName: String,
+  email: String,
+  password: String
+})
+const User = mongoose.model('user', userSchema);
 
-// const wss = new WebSocket.Server({ port: 8080 });
+const app = express();
+const port = 8080;
 
-// wss.on('connection', function connection(ws) {
-//   console.log('Connected');
-//   ws.on('message', function incoming(data) {
-//     let stringData = data.toString();
-//     if (stringData[0] === '#') {
-//       if (!db.keys[stringData.match(/#[a-z]+/i)]) {
-//         db.clients[ws.remotePort].send('Invalid command, list of commands:\n' + Object.keys(db.keys).join(',\n'));
-//         return false;
-//       } else {
-//         db.keys[stringData.match(/#[a-z]+/i)](stringData.trim(), ws.remotePort);
-//       }
-//       return true;
-//     } else if (db.chatRooms.length > 1) {
-//       let indexRoom;
-//       db.chatRooms.find((el, index) => {
-//         if (el[ws.remotePort]) {
-//           indexRoom = Object.keys(db.chatRooms[index]);
-//           return true;
-//         }
-//       });
-//       indexRoom.forEach(element => {
-//         if (element != ws.remotePort) {
-//           db.clients[element].send(ws.remotePort + ': ' + stringData);
-//         }
-//       });
-//     } else {
-//       ws.send('Choose your interlocutor:\n' + Object.keys(db.clients));
-//     }
-//   });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(bodyParser.json());
 
+app.post('/login', (req, res) => {
+  // console.log(req.body);
+  User.findOne({ email: req.body.email }, (err, data) => {
+    // console.log(err, data)
+    if (!data) {
+      res.send('no such client');
+      return;
+    }
+    res.send(data);
+  })
+});
 
-// ws.send('something');
-// });
+app.listen(port, () => {
+  console.log('listening port:', port);
+});
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   lastName: String,
+//   email: String,
+//   password: String
+// })
+// const User = mongoose.model('user', userSchema);
+
+//                                                                                                           crating new USER:
+// const user1 = new User({name: 'Bread', lastName: 'White', email: 'asd@gmail.com', password: '12345'});
+// user1.save((err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data, 'data');
+// })
+
+//                                                                      SOCKET
+const clientsEmails = [];
+
+User.find({}, (err, users) => {
+  if (err) return console.log(err);
+  users.forEach((element) => {
+    // console.log(element.email)
+    clientsEmails.push(element.email);
+  });
+});
+
+// console.log('aaaaaaaaaaaaaaaaaaaaa', findAllUsersEmail());
+server.listen(8000);
+
+io.on('connection', (client) => {
+  let name;
+  client.emit('connected', clientsEmails);
+  client.on('saveClient', (nickname) => {
+    if (nickname === false) {
+      return;
+    }
+    client.name = nickname;
+    console.log('client NICKNAME', client.name)
+    
+    // db.clients[nickname] = client.id;
+    // io.emit('changeClientsList', listOfUsers);
+    // console.log(findAllUsersEmail(), 'qwertyuioplkjhgfdsa');
+  })
+
+  client.broadcast.emit('changeClientsList', clientsEmails);
+
+  console.log('Socket connected');
+
+  client.on('message', function (data) {
+    // db.messages.push(data);
+    console.log(name, 'sended message');
+    const message = {
+      name: client.name,
+      mess: data.mess,
+      time: data.time,
+    }
+    client.broadcast.emit('message', message);
+  })
+
+  client.on('disconnect', (reason) => {
+    console.log(reason, name);
+    if (reason === 'io server disconnect') {
+      client.connect();
+    }
+  })
+})
